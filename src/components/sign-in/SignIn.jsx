46abfe5c5c5cd599737ce8bd45ac138a7ae52cb8
Arash Alaei <arashalaei@aut.ac.ir>
2021-08-16 04:21:57 +0430

    finishing sign in handleSubmit method

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import FormInput from './../form-input/FormInput'
 import Button from './../button/Button';
-import {signInWithGoogle} from './../../utils/firebase';
+import {auth ,signInWithGoogle} from './../../utils/firebase';
 
 class SignIn extends Component{
     constructor(){
@@ -13,6 +13,22 @@ class SignIn extends Component{
         }
     }
 
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        const {email, password} = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                email: '', 
+                password: '' 
+            })
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
     handleChange = (event) => {
         const {value, name} = event.target;
         this.setState({
@@ -29,9 +45,9 @@ class SignIn extends Component{
                     <h2 className="heading-secondary">I Already Have an Account</h2>
                     <span className="signin__heading-span">Sign in with your email and password</span>
                 </div>
-                <form action="#" className="form">
-                    <FormInput type="email" label="Email" handleChange={this.handleChange}/>
-                    <FormInput type="password" label="Password" handleChange={this.handleChange}/>
+                <form action="#" className="form" onSubmit={this.handleSubmit}>
+                    <FormInput type="email" label="Email" handleChange={this.handleChange} value={this.state.email}/>
+                    <FormInput type="password" label="Password" handleChange={this.handleChange} value={this.state.password}/>
                     <div className="form__group form__group-submit">
                         <Button color="blue">Sign in</Button>
                         <Button color="black" onClick={signInWithGoogle}>Sign in with Google</Button>
